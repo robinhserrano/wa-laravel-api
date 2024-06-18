80c4a701338782d643c05e2d29d88e7fa0f1e937
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-06-18 13:06:46 +0800

    Removed cors 2

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,6 @@ return Application::configure(basePath: dirname(__DIR__))
     )
     ->withMiddleware(function (Middleware $middleware) {
         $middleware->validateCsrfTokens(except:['/*']);
-        // $middleware->alias([
-        //     'cors' => \App\Http\Middleware\Cors::class,
-        // ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
