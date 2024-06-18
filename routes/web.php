48bfc9d19fc51f9c03c08d649e1174121b1d6a29
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-06-18 13:38:34 +0800

    Re-added welcome route in web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,8 @@
 <?php
 
-use App\Http\Controllers\SalesOrderController;
-use App\Models\SalesOrder;
 use Illuminate\Support\Facades\Route;
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
