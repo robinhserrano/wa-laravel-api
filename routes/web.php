78ce41d4daf95f58b96c5fdc4bae61884ff5dfbd
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-06-18 10:30:02 +0800

    Removed routes in web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use App\Http\Controllers\SalesOrderController;
 use App\Models\SalesOrder;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+// Route::get('/', function () {
+//     return view('welcome');
+// });
 
