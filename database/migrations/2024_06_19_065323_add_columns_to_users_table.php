fffcfe3a46556bb1e009fdfd8a4ae88ccb2d3d65
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-06-19 14:57:24 +0800

    Added migration add_columns_to_users_table

---
+++ database/migrations/2024_06_19_065323_add_columns_to_users_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->integer('access_level')->default(1);
+            $table->double('commission_split')->default(50.0); 
+            $table->unsignedBigInteger('sales_manager_id')->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('users', function (Blueprint $table) {
+            //
+        });
+    }
+};
