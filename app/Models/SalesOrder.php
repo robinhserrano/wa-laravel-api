d877150193b79c62c512adbbc3311fced96635d2
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-06-20 15:58:54 +0800

    Modified user() in SalesOrder controller 5

--- app/Models/SalesOrder.php
+++ app/Models/SalesOrder.php
@@ -18,8 +18,9 @@ class SalesOrder extends Model
 
     public function user()
     {
+        // No foreign key, filter by name (one-to-one relationship)
         return $this->belongsTo(User::class, 'x_studio_sales_rep_1', 'name');
-        //$salesRepName = $this->x_studio_sales_rep_1;
-        //return $this->hasOne(User::class, 'name', 'x_studio_sales_rep_1')->where('name', $salesRepName);
+        // $salesRepName = $this->x_studio_sales_rep_1;
+        // return $this->hasOne(User::class)->where('name', $salesRepName);
     }
 }
