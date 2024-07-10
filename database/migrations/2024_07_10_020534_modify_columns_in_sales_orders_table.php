75ec85ed1fe4c9037222ca63ddd80c86319764f0
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2024-07-10 10:06:47 +0800

    Made columns in salesOrderTable nullable

---
+++ database/migrations/2024_07_10_020534_modify_columns_in_sales_orders_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('sales_orders', function (Blueprint $table) {
+            $table->integer('last_manual_add_by')->nullable()->change();
+            $table->integer('last_confirmed_by')->nullable()->change();
+            $table->string('plain_text')->nullable()->change();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('sales_orders', function (Blueprint $table) {
+            //
+        });
+    }
+};
