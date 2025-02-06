4dc447ac512affc2fb062f294dd350ee22699d6f
robinhserrano <88170303+robinhserrano@users.noreply.github.com>
2025-02-06 14:28:08 +0800

    Fixed bulkStore in ProductStockController, added delete ProductStock not in newDisplayNames

--- app/Http/Controllers/ProductStockController.php
+++ app/Http/Controllers/ProductStockController.php
@@ -17,31 +17,50 @@ class ProductStockController extends Controller
     public function bulkStore(Request $request)
     {
         $allowedProduct = [
-            'display_name', 'categ_name', 'avg_cost', 'total_value', 'qty_available', 'free_qty', 'incoming_qty', 'outgoing_qty', 'virtual_available', 'warehouse_id',
+            'display_name',
+            'categ_name',
+            'avg_cost',
+            'total_value',
+            'qty_available',
+            'free_qty',
+            'incoming_qty',
+            'outgoing_qty',
+            'virtual_available',
+            'warehouse_id',
         ];
 
         $productList = $request->all();
         $newProducts = [];
+        $newDisplayNames = [];
 
         foreach ($productList as $product) {
             $filteredProduct = Arr::only($product, $allowedProduct);
-            $existingProduct = ProductStock::where('display_name', $filteredProduct['display_name'])->where('warehouse_id', $filteredProduct['warehouse_id'])->first();
+            $newDisplayNames[] = $filteredProduct['display_name'];
+
+            // Check if the product already exists
+            $existingProduct = ProductStock::where('display_name', $filteredProduct['display_name'])
+                ->where('warehouse_id', $filteredProduct['warehouse_id'])
+                ->first();
 
             if ($existingProduct) {
-
+                // Update existing product
                 $existingProduct->update($filteredProduct);
             } else {
-
+                // Add new product to the new products array
                 $newProducts[] = $filteredProduct;
             }
 
             $orderLines[] = $filteredProduct;
         }
 
+        // Create new products if any
         if (!empty($newProducts)) {
             foreach ($newProducts as $product) {
                 ProductStock::create($product);
             }
         }
+
+        // Delete existing products where display_name is not in the new list
+        ProductStock::whereNotIn('display_name', $newDisplayNames)->delete();
     }
 }
